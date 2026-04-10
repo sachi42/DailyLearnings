eb9bc5a7a692794df620c0d32b904423b5d6afb2
sachi tripathi <61285008+sachi42@users.noreply.github.com>
2026-04-10 22:21:42 +0530

    Time: 1 ms (64.71%) | Memory: 44.1 MB (52.24%) - LeetSync

--- 4179-largest-even-number/largest-even-number.java
+++ 4179-largest-even-number/largest-even-number.java
@@ -1,6 +1,7 @@
 class Solution {
     public String largestEven(String s) {
         int index = -1;
+        //we will be mov ing from right to left, since we need to chec k the values from right to extreme left
         for (int i=s.length()-1; i>=0; i--) {
             if (s.charAt(i) == '2') {
                 index = i;
